d33d61ba71eec1610eb53db8f8ae04ffb5a00a59
youijchoi <dev.youjin.choi@gmail.com>
2017-11-07 00:23:17 +0900

    add comment about React in Header

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,9 @@ class Header extends Component {
     return (
 		<section className="page-header">
 	      <h1 className="project-name">Developer Youjin Choi</h1>
-	      <h2 className="project-tagline">Hi! I've been working as a full-stack web developer since 2013.</h2>
+	      <h2 className="project-tagline">Hello! I've been working as a full-stack web developer since 2013.<br/>
+	      	This page uses React JS!
+	      </h2>
 	        <Link to="/" className="btn">Home</Link>
 	        <a href="https://github.com/youjinchoi" className="btn">GitHub</a>
 	        <Link to="/profile" className="btn">Profile</Link>
